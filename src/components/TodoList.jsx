5b53b5e167feb78a36945c464d9c6b06e928d08c
Alperen <akpinar_974@hotmail.com>
2024-08-19 17:14:20 +0300

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Todo from './Todo'
+import '../css/TodoList.css'
+
+function TodoList({ todos, onRemoveTodo, onUpdateTodo }) {
+    return (
+        <div className='todo-list'>
+
+            {
+                todos && todos.map((todo) => (
+                    <Todo key={todo.id} todo={todo} onRemoveTodo={onRemoveTodo} onUpdateTodo={onUpdateTodo} />
+                ))
+            }
+        </div>
+    )
+}
+
+export default TodoList
